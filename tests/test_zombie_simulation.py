2eca04f328390750d17bd5089f912de253b8ac14
FarDust <gnfaundez@uc.cl>
2025-04-02 11:55:58 -0300

    tests: remove duplicated test

--- tests/test_zombie_simulation.py
+++ tests/test_zombie_simulation.py
@@ -35,16 +35,6 @@ def test_clean_room_removes_infection():
     assert (0, 0) not in env.state.infected_coords
 
 
-def test_reset_sensor():
-    building = Building.from_2d_floor_spec(1, 1)
-    infected = {(0, 0): {ZOMBIE_COUNT: 1}}
-    state = ZombieSimulationState(building=building, infected_coords=infected)
-    env = ZombieEnvironment(state)
-
-    env.reset_sensor(0, 0)
-    assert env.state.building.floors[0].rooms[0].sensor.status == "normal"
-
-
 def test_sensor_alert_on_startup(zombie_env):
     env = zombie_env(1, 1, {(0, 0): {ZOMBIE_COUNT: 1}})
     assert env.state.building.floors[0].rooms[0].sensor.status == "alert"
